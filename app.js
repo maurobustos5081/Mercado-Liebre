5b390cc75a6d1477adfcbde62d7eeb06eeb91d1c
maurobustos5081 <mauro_18_b@hotmail.com>
2023-07-30 17:43:38 -0300

    se crea diseño adaptativo

--- app.js
+++ app.js
@@ -10,9 +10,9 @@ app.use( express.static(publicFolderPath) ); */
 
 app.use(express.static('public'));
 
-app.listen(PORT, ()=>{
-    console.log(`Servidor corriendo en el puerto ${PORT}`);
-});
+app.listen(PORT, () =>
+  console.log("Server running in http://localhost:" + PORT)
+);
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'));

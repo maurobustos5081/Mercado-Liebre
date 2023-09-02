b152e6ee28054e2be07ed60882900b601394be36
maurobustos5081 <mauro_18_b@hotmail.com>
2023-09-02 20:37:31 -0300

    Se crea CRUD

---
+++ CRUD/materiales/src/controllers/mainController.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+const controller = {
+	index: (req, res) => {
+		return res.render('index', {
+			visited : products.filter(product => product.category === 'visited'),
+			sale : products.filter(product => product.category === 'in-sale'),
+			//products,
+			toThousand
+		})
+	},
+	search: (req, res) => {
+		// Do the magic
+		const keywords = req.query.keywords
+		const results = products.filter(product => product.name.toLowerCase().includes(keywords.toLowerCase()))
+		return res.render('results', {
+			results, 
+			toThousand,
+			keywords
+		})
+
+	},
+};
+
+module.exports = controller;
